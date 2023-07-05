5c399a4b546c809cccf7d1c5272a31543721a2f3
yungNATE <nathan.martinigol@gmail.com>
2023-07-05 12:51:11 +0200

    index page

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,35 @@
 import '../node_modules/youtube-background/src/main.js';
 
 new VideoBackgrounds('[data-vbg]');
+
+// Magnet buttons
+document.querySelectorAll('.gravityButton').forEach(btn => {
+  
+    btn.addEventListener('mousemove', (e) => {
+      
+      const rect = btn.getBoundingClientRect();    
+      const h = rect.width / 2;
+      
+      const x = e.clientX - rect.left - h;
+      const y = e.clientY - rect.top - h;
+  
+      const r1 = Math.sqrt(x*x+y*y);
+      const r2 = (1 - (r1 / h)) * r1;
+  
+      const angle = Math.atan2(y, x);
+      const tx = Math.round(Math.cos(angle) * r2 * 100) / 100;
+      const ty = Math.round(Math.sin(angle) * r2 * 100) / 100;
+      
+      const op = (r2 / r1) + 0.25;
+  
+      btn.style.setProperty('--tx', `${tx}px`);
+      btn.style.setProperty('--ty', `${ty}px`);
+      btn.style.setProperty('--opacity', `${op}`);
+    });
+  
+    btn.addEventListener('mouseleave', (e) => {
+      btn.style.setProperty('--tx', '0px');
+      btn.style.setProperty('--ty', '0px');
+      btn.style.setProperty('--opacity', `${0.25}`);
+    });
+  })
