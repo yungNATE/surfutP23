5f3796d81c79f3ad4619349245f1d39a3d9fd28e
yungNATE <nathan.martinigol@gmail.com>
2023-07-11 18:47:37 +0200

    tentative récupération images

--- JS/galerie.js
+++ JS/galerie.js
@@ -2,16 +2,16 @@
 let webAppID = "AKfycbwR1gOmKKWQlaRSkVI63lIzP_n37iwJB0P8Ij7or9_rUSzWSBv8vqg6svQfYpLtiZbveA";
 let webAppUrl = `https://script.google.com/macros/s/${webAppID}/exec`;
 
-async function fetchData(webAppUrl) {
+async function fetchData(url) {
     try {
-      const response = await fetch(webAppUrl);
+      const response = await fetch(url);
       const data = await response.json();
       
       console.log(data);
-
+      
     } catch (error) {
       console.error(error);
     }
 }
   
-fetchData();
+fetchData(webAppUrl);
