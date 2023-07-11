9b59854e72bc2ec40e0b3ae7db170eb4070ca465
yungNATE <nathan.martinigol@gmail.com>
2023-07-11 22:42:48 +0200

    arriver à lazy loader les images

--- JS/galerie.js
+++ JS/galerie.js
@@ -1,17 +1,54 @@
 // Get all photos from media folder
-let webAppID = "AKfycbwR1gOmKKWQlaRSkVI63lIzP_n37iwJB0P8Ij7or9_rUSzWSBv8vqg6svQfYpLtiZbveA";
+let webAppID = "AKfycbwdCUqGrw5te7hVBo74GNy91aDo2-tKB0Y9e9KLSzWiNM_C7OTliaurBOHl8qdLUFXz";
 let webAppUrl = `https://script.google.com/macros/s/${webAppID}/exec`;
 
 async function fetchData(url) {
+    console.info("Fetching data from " + url);
     try {
-      const response = await fetch(url);
-      const data = await response.json();
-      
-      console.log(data);
-      
+        const response = await fetch(url);
+        const responseJSON = await response.json();
+        const data = responseJSON.data;
+    
+        console.log(data[Object.keys(data)[0]]);
+
+        return data[Object.keys(data)[0]]; // permet d'évacuer le dossier racine
+       
     } catch (error) {
-      console.error(error);
+        alert("Une erreur est survenue, recharge la page.");
+        console.error(error);
     }
 }
   
-fetchData(webAppUrl);
+
+fetchData(webAppUrl).then((data) => { document.querySelector('.gallery').innerHTML = buildGallery(data) });
+
+function buildGallery(data, level = 2) {
+    var galleryHTML = '';
+  
+    for (var key in data) {
+      if (Object.keys(data[key]).length === 0) {
+        continue; // Ignorer les dossiers vides
+      }
+  
+      galleryHTML += `<div class="folder"><h${level}>${key}</h${level}>`;
+  
+      if (typeof data[key].photos !== 'undefined') {
+        galleryHTML += '<div class="photo-gallery">';
+  
+        for (var i = 0; i < data[key].photos.length; i++) {
+          var photo = data[key].photos[i];
+        //   galleryHTML += '<img src="https://drive.google.com/drive/folders/' + photo.id + '" alt="' + photo.name + '">';
+          galleryHTML += `<img src="https://drive.google.com/uc?export=view&id=${photo.id}" loading="lazy" alt="${photo.name}">`;
+        }
+  
+        galleryHTML += '</div>';
+      } else {
+        galleryHTML += buildGallery(data[key], level + 1);
+      }
+  
+      galleryHTML += '</div>';
+    }
+  
+    return galleryHTML;
+}
+  
