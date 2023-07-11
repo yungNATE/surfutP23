dc081cf55e712f9b06743ffdfc2d3a6104a9c05e
yungNATE <nathan.martinigol@gmail.com>
2023-07-11 03:13:23 +0200

    tentative récupération images drive

--- JS/galerie.js
+++ JS/galerie.js
@@ -1,1 +1,17 @@
 // Get all photos from media folder
+let webAppID = "AKfycbwR1gOmKKWQlaRSkVI63lIzP_n37iwJB0P8Ij7or9_rUSzWSBv8vqg6svQfYpLtiZbveA";
+let webAppUrl = `https://script.google.com/macros/s/${webAppID}/exec`;
+
+async function fetchData(webAppUrl) {
+    try {
+      const response = await fetch(webAppUrl);
+      const data = await response.json();
+      
+      console.log(data);
+
+    } catch (error) {
+      console.error(error);
+    }
+}
+  
+fetchData();
